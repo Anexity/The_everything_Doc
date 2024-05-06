08f33f2a66804741c5d8d1fe39539c1d4e05f388
Nex <89796047+Anexity@users.noreply.github.com>
2024-05-06 18:39:24 +0200

    push offline code
    it begins again

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function loadBlog() {
+    document.getElementById("loader").innerHTML = '<object class="fadein" id="mask" type="text/html" data="./sits/blog.html"></object>';
+}
+
+function loadIntel() {
+    let data = "This is a test"
+    document.getElementById("loader").innerHTML = data;
+}
+
+function loadDL() {
+    let data = "This is a test"
+    document.getElementById("loader").innerHTML = data;
+}
